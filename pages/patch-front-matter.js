448d533fbac78cd56306c2eb14755e86d6c7d533
Kerry Snyder <kerry@atmanaut.me>
2021-04-02 23:29:04 -0700

    oops touched all the files again patched mdx without frontmatter got eslint behaving

--- pages/patch-front-matter.js
+++ pages/patch-front-matter.js
@@ -1,37 +1,30 @@
 import path from 'path'
 import Layout from '../components/Layout'
-import { patchMissingFrontMatter } from '../lib/mdx'
+import { patchMissingFrontMatter } from '../lib/frontmatter'
 
-const root = process.cwd()
+const root = process.cwd();
 
-export default function Test ({ files, filesFixed }) {
+
+export default function Test ({ filesFixed }) {
 	return (
 		<Layout>
 			<h1>Patching Front Matter</h1>
-			<div className="row flex">
-				<div className="col flex">
+			<div className="flex row">
+				<div className="flex col">
 					<h2>Files Fixed</h2>
-					<ul>
+					<ul className="microlist">
 						{filesFixed.map((file, index) => (
 							<li key={index}>{file}</li>
 						))}
 					</ul>
 				</div>
-				{/* <div className="col half flex">
-					<h2>Files Scanned</h2>
-					<ul>
-						{files.map((file) => (
-							<li>{file}</li>
-						))}
-					</ul>
-				</div> */}
 			</div>
 		</Layout>
 	)
 }
 
 export async function getStaticProps () {
-	const dirPath = path.join(root, '_notes', 'journal')
+	const dirPath = path.join(root, '_notes')
 	const results = await patchMissingFrontMatter(dirPath)
 
 	return {
