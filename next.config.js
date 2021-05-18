aa0e8f6e5fb15afaad9714923d8ad4f456e6f800
Kerry Snyder <kerry@atmanaut.me>
2021-05-18 00:12:58 -0700

    trying a couple of tina approaches

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withPWA = require('next-pwa')
+require('dotenv').config()
 
 module.exports = withPWA({
+	env: {
+		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+		REPO_FULL_NAME: process.env.REPO_FULL_NAME,
+		BASE_BRANCH: process.env.BASE_BRANCH,
+	},
 	pwa: {
 		dest: 'public',
 	},
